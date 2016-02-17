206bb715216f2f6b0b271657d35e82ca0343daf5
rdifelice <remo.difelice@studio.unibo.it>
2016-02-17 21:32:18 +0100

    Primi test

--- camera/server/handlers/resHandler.js
+++ camera/server/handlers/resHandler.js
@@ -17,18 +17,10 @@ function faceDetection(req, res, next) {
         if (im) {
           im.detectObject(cv.FACE_CASCADE, {}, function(err, faces) {
             if (faces) {
-              /*
-              for (var i = 0; i < faces.length; i++) {
-                var x = faces[i]
-                im.ellipse(x.x + x.width / 2, x.y + x.height / 2, x.width / 2, x.height / 2);
-              }
-              im.save('../img/out.png');
-              */
               console.log(faces)
               res.status(200).send(faces)
               return next();
             }
-            //else console.log(err)
           });
         }
       })
